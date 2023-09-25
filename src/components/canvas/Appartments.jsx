f4bf8e9957397980b9dee6bf88e7e618c993e18c
Luc Audemard <bjluc@bjluc.co.uk>
2023-09-25 23:19:16 +0100

    First commit

---
+++ src/components/canvas/Appartments.jsx
@@ -0,0 +1,78 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Appartments = ({ isMobile }) => {
+  const appartment = useGLTF("./petit_appart/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={1.5} groundColor='black' />
+      <spotLight
+        position={[120, 70, 70]}
+        angle={0.9}
+        penumbra={2}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={appartment.scene}
+        scale={isMobile ? 0.2 : 0.3}
+        position={isMobile ? [-1, -0.7, 2.3] : [-1, -1.45, 3]}
+        rotation={[0, 1.25, 0]}
+      />
+    </mesh>
+  );
+};
+
+const AppartmentsCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 600px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [15, 2, 5], fov: 20 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Appartments isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default AppartmentsCanvas;
---
+++ src/components/canvas/index.js
@@ -0,0 +1,13 @@
+import EarthCanvas from './Earth'
+import BallCanvas from './Ball'
+import ComputersCanvas from './Computers'
+import StarsCanvas from './Stars'
+import AppartmentsCanvas from './Appartments'
+
+export {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+  AppartmentsCanvas,
+}
